e84332bd33e52bba7af87d85e2195d8e03aae789
serene-x <serenexue4@gmail.com>
2026-02-23 23:43:23 -0500

    Change suggest button text to 'Can't find your club?'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -178,7 +178,7 @@ function SuggestClubButton() {
           <line x1="12" y1="5" x2="12" y2="19" />
           <line x1="5" y1="12" x2="19" y2="12" />
         </svg>
-        Suggest a Club
+        Can't find your club?
       </button>
 
       {/* Modal backdrop + dialog */}
